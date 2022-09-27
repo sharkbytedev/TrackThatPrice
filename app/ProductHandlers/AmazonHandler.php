ab568be1ae350232278b88be90b60ebf132768a9
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-09-27 11:00:28 -0400

    Completed AmazonHandler

--- app/ProductHandlers/AmazonHandler.php
+++ app/ProductHandlers/AmazonHandler.php
@@ -2,22 +2,25 @@
 
 namespace App\ProductHandlers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
 use Goutte\Client;
-
-use App\ProductHandlers\ProductHandler;
 use InvalidArgumentException;
 
-class AmazonHandler extends ProductHandler {
-    public const store_name = "amazon";
+class AmazonHandler extends ProductHandler
+{
+    public const store_name = 'amazon';
+
     protected $url_base = [
-        "https://www.amazon.ca/",
-        "https://www.amazon.com/",
+        'https://www.amazon.ca/',
+        'https://www.amazon.com/',
 
     ];
 
-    function __construct(string $url)
+    public function __construct(string $url, Product $product = null)
     {
+        if (isset($product) && $product->store != $this::store_name) {
+            throw new \Exception('Store must be amazon');
+        }
         $valid = false;
         // Check if the url has a valid base for this store
         foreach ($this->url_base as $u) {
@@ -26,36 +29,52 @@ class AmazonHandler extends ProductHandler {
                 break;
             }
         }
-        if (!$valid) {
+        if (! $valid) {
             throw new \Exception("'$url' has an invalid base");
         }
         $this->product_url = $url;
 
+        if ($product) {
+            $this->_obj = $product;
+            $this->name = $product->product_name;
+            $this->image_url = $product->image_url;
+            $this->last_updated = $product->last_queried;
+            $this->price = $product->price;
+        }
     }
-    // TODO: Add error checking
-    function _update(): Array
+
+    public static function fromDbModel(Product $product): ProductHandler
+    {
+        return new AmazonHandler($product->product_url, $product);
+    }
+
+    public function update(bool $update_db = false): array
     {
         $client = new Client();
-        $client->setServerParameter("HTTP_USER_AGENT", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36");
-        $website = $client->request("GET", $this->product_url);
-        
+        $client->setServerParameter('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36');
+        $website = $client->request('GET', $this->product_url);
+
         try {
             // Gets the text of the first div with the id #productTitle
-            $this->name = $website->filter("#productTitle")->eq(0)->text();
+            $this->name = $website->filter('#productTitle')->eq(0)->text();
             // On amazon, the price is divided into 3 parts: Symbol, whole, and fraction
-            $price_whole = $website->filter(".a-price-whole")->eq(0)->text();
-            $price_fraction = $website->filter(".a-price-fraction")->eq(0)->text();
+            $price_whole = $website->filter('.a-price-whole')->eq(0)->text();
+            $price_fraction = $website->filter('.a-price-fraction')->eq(0)->text();
             $this->price = floatval($price_whole.$price_fraction);
-        
+
             // Get an image url. Often on Amazon there's more than one, so we'll just get the first one.
-            $this->image_url = $website->filter("#imgTagWrapperId")->filter("img")->eq(0)->attr("src");
-        }
-        catch (InvalidArgumentException $e) {
+            $this->image_url = $website->filter('#imgTagWrapperId')->filter('img')->eq(0)->attr('src');
+        } catch (InvalidArgumentException $e) {
             return [false, $e];
         }
         $this->last_updated = new \DateTime();
+        
+        if (isset($this->_obj)) {
+            $this->_obj->last_status_code = $client->getResponse()->getStatusCode();
+            $this->_update_db_model($update_db);
+        }
 
         // No errors were caught, so return true
-        return [true, ""];
+        return [true, ''];
     }
 }
--- app/ProductHandlers/ProductHandler.php
+++ app/ProductHandlers/ProductHandler.php
@@ -2,40 +2,92 @@
 
 namespace App\ProductHandlers;
 
+use App\Models\Product as ModelsProduct;
+
 // Class for handling updating products
-abstract class ProductHandler {
+abstract class ProductHandler
+{
+    protected const store_name = "";
+
     protected $price;
+
     protected $name;
+
     protected $last_updated;
+
     protected $image_url;
+
     protected $product_url;
+
     protected $_obj;
-    
-    public function get_price(): float {
+
+    public function get_price(): float
+    {
         return $this->price;
     }
-    public function get_name(): string {
+
+    public function get_name(): string
+    {
         return $this->name;
     }
-    public function get_product_url(): string {
+
+    public function get_product_url(): string
+    {
         return $this->product_url;
     }
-    public function get_last_updated(): \DateTime {
+
+    public function get_last_updated(): \DateTime
+    {
         return $this->last_updated;
     }
-    public function get_image_url(): ?string {
+
+    public function get_image_url(): ?string
+    {
         return $this->image_url;
     }
-    public function get_as_json(): string {
+
+    public function get_as_json(): string
+    {
         return json_encode([
-            "name"=>$this->name,
-            "price"=>$this->price,
-            "last_updated"=>$this->last_updated,
-            "product_url"=>$this->product_url,
-            "image_url"=>$this->image_url
+            'name' => $this->name,
+            'price' => $this->price,
+            'last_updated' => $this->last_updated,
+            'product_url' => $this->product_url,
+            'image_url' => $this->image_url,
         ]);
     }
-    // Should return a 2 element array. The first element should be a boolean, 
+
+    // Should return a 2 element array. The first element should be a boolean,
     // and the second should be an exception
-    abstract public function _update(): Array;
+    abstract public function update(bool $update_db = false): array;
+
+    abstract public static function fromDbModel(ModelsProduct $product): ProductHandler;
+
+    public function createDbEntry(int $update_interval = 3600, bool $save = false): void
+    {
+        $product = new ModelsProduct([
+            'product_name' => $this->name,
+            'product_url' => $this->product_url,
+            'store' => $this::store_name,
+            'update_interval' => $update_interval,
+            'price' => $this->price
+        ]);
+        $this->_obj = $product;
+        if ($save) {
+            $product->save();
+        }
+    }
+
+    protected function _update_db_model(bool $save = false): void
+    {
+        $this->_obj->product_name = $this->name;
+        $this->_obj->product_url = $this->product_url;
+        $this->_obj->image_url = $this->image_url;
+        $this->_obj->last_queried = $this->last_updated;
+        $this->_obj->store = $this::store_name;
+        $this->_obj->price = $this->price;
+        if ($save) {
+            $this->_obj->save();
+        }
+    }
 }
