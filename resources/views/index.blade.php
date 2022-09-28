1d26289b34eb9d282900eb1d8e04418b9b536110
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-09-28 10:31:57 -0400

    Removed WalmartHandler, moving to its own branch

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,14 +2,8 @@
 
 use App\Http\Controllers\ScraperController;
 use App\ProductHandlers\AmazonHandler;
-use App\ProductHandlers\WalmartHandler;
 use App\Models\Product;
 
-$p = new WalmartHandler("https://www.walmart.ca/en/ip/sony-srsxb13b-xb13-extra-bass-compact-bluetooth-speaker-black-black/6000202817704");
-dump($p->update());
-$p->createDbEntry();
-$p->update(true);
-dump($p);
 // $p = Product::all()[1];
 // $h = AmazonHandler::fromDbModel($p);
 // $h->update(true);
