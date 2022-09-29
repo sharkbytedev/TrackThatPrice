141bde0a3be446609572e0b26ea37f8f34f2a361
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-09-29 11:57:31 -0400

    Style fixes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,22 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Product extends Model
 {
     use HasFactory, HasUuids;
 
     protected $fillable = [
-        "product_url",
-        "product_name",
-        "update_interval",
-        "store"
+        'product_url',
+        'product_name',
+        'update_interval',
+        'store',
     ];
 
-    public function users() {
-        return $this->belongsToMany(User::class, "product_users");
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'product_users');
     }
 }
