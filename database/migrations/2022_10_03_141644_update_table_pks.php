12b77e5ed28a04040e1d5f66e692b33c002278da
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-10-03 10:24:28 -0400

    Changed id column of products and product_user to PK

---
+++ database/migrations/2022_10_03_141644_update_table_pks.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->uuid('id')->primary()->change();
+        });
+
+        Schema::table('product_user', function (Blueprint $table) {
+            $table->uuid('id')->primary()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->uuid('id')->change();
+        });
+
+        Schema::table('product_user', function (Blueprint $table) {
+            $table->uuid('id')->change();
+        });
+    }
+};
