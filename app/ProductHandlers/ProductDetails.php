d72b9baa9e3668473b4da7ddc6dbf47dba883916
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-10-05 09:39:52 -0400

    Added from_db_entry method

--- app/ProductHandlers/ProductDetails.php
+++ app/ProductHandlers/ProductDetails.php
@@ -2,6 +2,8 @@
 
 namespace App\ProductHandlers;
 
+use App\Models\Product;
+
 class ProductDetails
 {
     public int $price;
@@ -13,6 +15,18 @@ class ProductDetails
     public string $product_url;
 
     public int $status_code;
+
+    public static function from_db_entry(Product $p): ProductDetails
+    {
+        $details = new ProductDetails();
+        $details->price = $p->price;
+        $details->name = $p->product_name;
+        $details->image_url = $p->image_url;
+        $details->product_url = $p->product_url;
+        $details->status_code = $p->last_status_code;
+
+        return $details;
+    }
 
     public function get_as_json(): string
     {
