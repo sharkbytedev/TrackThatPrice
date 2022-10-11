7342ddaacecc6f6edf97903f42b762182248f58a
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-10-11 11:15:51 -0400

    Added http status code handling

--- app/ProductHandlers/AmazonHandler.php
+++ app/ProductHandlers/AmazonHandler.php
@@ -4,6 +4,8 @@ namespace App\ProductHandlers;
 
 use App\Models\Product;
 use Goutte\Client;
+use InvalidArgumentException;
+use App\Exceptions\QueryExceptions\{NotFoundError, ServerError, BadRequestError, GoneError, QueryException};
 
 class AmazonHandler implements ProductHandler
 {
@@ -11,6 +13,27 @@ class AmazonHandler implements ProductHandler
         'https://www.amazon.ca/',
         'https://www.amazon.com/',
     ];
+
+    protected static function handleStatusCode(int $code) {
+        switch ($code) {
+            case 200:
+                break;
+            case 500:
+                throw new ServerError("Server responded with 500");
+                break;
+            case 404:
+                throw new NotFoundError("Server responded with 404");
+                break;
+            case 410:
+                throw new GoneError("Server responded with 410");
+            case 400:
+                throw new BadRequestError("Server responded with 500");
+                break;
+            default:
+                throw new QueryException("Server responded with non-200 status code $code");
+                break;
+        }
+    }
 
     protected string $product_url;
 
@@ -20,6 +43,7 @@ class AmazonHandler implements ProductHandler
         $client = new Client();
         $client->setServerParameter('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36');
         $website = $client->request('GET', $url);
+        AmazonHandler::handleStatusCode($client->getResponse()->getStatusCode());
 
         $details = new ProductDetails();
 
@@ -31,8 +55,13 @@ class AmazonHandler implements ProductHandler
         // Store price in cents, so multiply price by 100
         $details->price = floatval($price_whole.$price_fraction) * 100;
 
-        // Get an image url. Often on Amazon there's more than one, so we'll just get the first one.
-        $details->image_url = $website->filter('#imgTagWrapperId')->filter('img')->eq(0)->attr('src');
+        try {
+            // Get an image url. Often on Amazon there's more than one, so we'll just get the first one.
+            $details->image_url = $website->filter('#imgTagWrapperId')->filter('img')->eq(0)->attr('src');
+        }
+        catch (InvalidArgumentException $e) {
+            $details->image_url = null;
+        }
 
         return $details;
     }
