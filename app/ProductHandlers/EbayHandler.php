31d3b6b93a90e2e62e4f72cfd509f1be9f4b4578
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-10-13 10:13:34 -0400

    Updated EbayHandler

--- app/ProductHandlers/EbayHandler.php
+++ app/ProductHandlers/EbayHandler.php
@@ -4,9 +4,13 @@ namespace App\ProductHandlers;
 
 use App\Models\Product;
 use Goutte\Client;
-use InvalidArgumentException;
+use App\Exceptions\QueryExceptions\BadRequestError;
+use App\Exceptions\QueryExceptions\GoneError;
+use App\Exceptions\QueryExceptions\NotFoundError;
+use App\Exceptions\QueryExceptions\QueryException;
+use App\Exceptions\QueryExceptions\ServerError;
 
-class EbayHandler extends ProductHandler
+class EbayHandler implements ProductHandler
 {
     public const store_name = 'ebay';
 
@@ -16,42 +20,51 @@ class EbayHandler extends ProductHandler
 
     ];
 
-    public static function from_db_model(Product $product): ProductHandler
+    protected static function handleStatusCode(int $code)
     {
-        return new EbayHandler($product->product_url, $product);
+        switch ($code) {
+            case 200:
+                break;
+            case 500:
+                throw new ServerError('Server responded with 500');
+                break;
+            case 404:
+                throw new NotFoundError('Server responded with 404');
+                break;
+            case 410:
+                throw new GoneError('Server responded with 410');
+            case 400:
+                throw new BadRequestError('Server responded with 500');
+                break;
+            default:
+                throw new QueryException("Server responded with non-200 status code $code");
+                break;
+        }
     }
 
-    public function update(bool $update_db = false): array
+    public static function crawl(Product $product): ProductDetails
     {
         $client = new Client();
         $client->setServerParameter('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36');
-        $website = $client->request('GET', $this->product_url);
-        try {
-            $this->name = $website->filter('h1[class="x-item-title__mainTitle"] > span')->eq(0)->text();
-            $price_text = explode('$', $website->filter('#prcIsum')->eq(0)->text());
-            $this->price = floatval(end($price_text));
+        $website = $client->request('GET', $product->product_url);
+        EbayHandler::handleStatusCode($client->getResponse()->getStatusCode());
+        
+        $details = new ProductDetails();
+        $details->name = $website->filter('h1[class="x-item-title__mainTitle"] > span')->eq(0)->text();
+        $price_text = explode('$', $website->filter('#prcIsum')->eq(0)->text());
+        $details->price = floatval(end($price_text)) * 100;
 
-            // On ebay, the image element is created by a short bit of js that contains the image source
-            $img_script_text = $website->filter('#mainImgHldr > script')->eq(0)->text();
+        // On ebay, the image element is created by a short bit of js that contains the image source
+        $img_script_text = $website->filter('#mainImgHldr > script')->eq(0)->text();
 
-            // Regex to match the image URL from the text of the script
-            $url_pattern = '#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#';
-            $matches = [];
-            preg_match($url_pattern, $img_script_text, $matches);
-            $this->image_url = $matches[0];
+        // Regex to match the image URL from the text of the script
+        $url_pattern = '#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#';
+        $matches = [];
+        preg_match($url_pattern, $img_script_text, $matches);
+        $details->image_url = $matches[0];
 
-            // An InvalidArgumentException means one of the properties(name, price, etc.) could not be found
-        } catch (InvalidArgumentException $e) {
-            return [false, $e];
-        }
-        $this->last_updated = new \DateTime();
-
-        if (isset($this->_obj)) {
-            $this->_obj->last_status_code = $client->getResponse()->getStatusCode();
-            $this->_update_db_model($update_db);
-        }
 
         // No errors were caught, so return true
-        return [true, ''];
+        return $details;
     }
 }
