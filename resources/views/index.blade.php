0db308d53301ab07cd881df670ed107f490d8d6d
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-10-18 09:49:41 -0400

    Added some placeholder icons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,17 +14,26 @@
             <div id="features">
                 <h3 class="text-xl"><b>Features</b></h3>
                 <div class="sm:flex flex-column justify-center background-sky-500 w-4/5 text-center m-auto">
-                    <div class="border-2 py-1 px-2 w-full sm:w-1/3">
+                    <div class="border-0 py-1 px-2 w-full sm:w-1/3 m-2 rounded-xl shadow-md bg-slate-50 flex-none">
                         <b>Get notified</b>
                         <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, porro?</p>
+                        <div class="justify-center flex text-center w-full">
+                            <img class="margin-auto" src="/images/bell.svg" alt="" srcset="">
+                        </div>
                     </div>
-                    <div class="border-2 py-1 px-2 w-full sm:w-1/3">
+                    <div class="border-0 py-1 px-2 w-full sm:w-1/3 m-2 rounded-xl shadow-md bg-slate-50 flex-none">
                         <b>Wide support</b>
                         <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, porro?</p>
+                        <div class="justify-center flex text-center w-full">
+                            <img class="margin-auto" src="/images/tool.svg" alt="" srcset="">
+                        </div>
                     </div>
-                    <div class="border-2 py-1 px-2 w-full sm:w-1/3">
+                    <div class="border-0 py-1 px-2 w-full sm:w-1/3 m-2 rounded-xl shadow-md bg-slate-50 flex-none">
                         <b>Historical data</b>
                         <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, porro?</p>
+                        <div class="justify-center flex text-center w-full">
+                            <img class="margin-auto" src="/images/bar-chart-2.svg" alt="" srcset="">
+                        </div>
                     </div>
                 </div>
             </div>
