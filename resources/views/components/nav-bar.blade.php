cc62ebabaf469fc73429366b98b988b0d6f8c653
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-10-18 11:14:51 -0400

    Added dark mode support

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{open: false}" class="flex flex-row w-full bg-white border-b-2 fixed">
+<div x-data="{open: false}" class="flex flex-row w-full dark:border-slate-600 dark:bg-slate-800 dark:text-slate-200 text-black bg-white border-b-2 fixed">
     {{-- Title --}}
     <div class="p-3 w-48 flex-none h-full justify-start">
         <div class="text-center mr-2 px-1 h-full">
@@ -7,29 +7,29 @@
     </div>
     {{-- Nav links --}}
     <div class="hidden sm:flex w-full h-full flex-row">
-        <div class="flex-none w-32 text-center border-x-2 py-3">
-            <x-nav-link :href="'#about'" class="text-black">
+        <div class="flex-none w-32 text-center border-x-2 py-3 dark:border-slate-600">
+            <x-nav-link :href="'#about'" class="dark:text-slate-200 text-black">
                 About
             </x-nav-link>
         </div>
-        <div class="flex-none w-32 text-center border-r-2 py-3">
-            <x-nav-link :href="'#prices'" class="text-black">
+        <div class="flex-none w-32 text-center border-r-2 py-3 dark:border-slate-600">
+            <x-nav-link :href="'#prices'" class="dark:text-slate-200 text-black">
                 Prices
             </x-nav-link>
         </div>
     </div>
     
     {{-- Login/register button --}}
-    <div class="fixed top-0 right-0 sm:static py-2 px-1 w-48 flex-none justify-end sm:justify-center sm:h-full border-l-2">
+    <div class="fixed top-0 right-0 sm:static py-2 px-1 w-48 flex-none justify-end sm:justify-center sm:h-full border-l-2  dark:border-slate-600">
         <div class="text-center mr-2 px-1 h-full flex-row flex">
             
             <a href="{{ route("register") }}" class="flex-none">
-                <x-primary-button class="flex-none mr-1">
+                <x-primary-button class="flex-none  dark:border-slate-200 mr-1">
                     <b>Sign up</b>
                 </x-primary-button>
             </a>
             <a href="{{ route("login") }}" class="flex-none">
-                <x-primary-button class="flex-none">
+                <x-primary-button class="flex-none  dark:border-slate-200">
                     <b>Log in</b>
                 </x-primary-button>
             </a>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <x-nav-bar/>
-    <div>
+    <div class="dark:bg-slate-800 dark:text-slate-200">
         <br>
         <br>
         <br>
@@ -17,25 +17,25 @@
             <div id="features">
                 <h3 class="text-xl"><b>Features</b></h3>
                 <div class="sm:flex flex-column justify-center w-4/5 text-center m-auto">
-                    <div class="border-0 p-4 w-full sm:w-1/3 m-2 rounded-xl shadow-md bg-slate-50 flex-none">
+                    <div class="border-0 p-4 w-full sm:w-1/3 m-2 rounded-xl shadow-md bg-slate-700 flex-none">
                         <b>Get notified</b>
                         <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, porro?</p>
                         <div class="justify-center flex text-center w-full">
-                            <img class="select-none" src="/images/bell.svg" alt="" srcset="">
+                            <img class="select-none m-2" src="/images/bell.svg" alt="" srcset="">
                         </div>
                     </div>
-                    <div class="border-0 p-4 w-full sm:w-1/3 m-2 rounded-xl shadow-md bg-slate-50 flex-none">
+                    <div class="border-0 p-4 w-full sm:w-1/3 m-2 rounded-xl shadow-md bg-slate-700 flex-none">
                         <b>Wide support</b>
                         <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, porro?</p>
                         <div class="justify-center flex text-center w-full">
-                            <img class="select-none" src="/images/tool.svg" alt="" srcset="">
+                            <img class="select-none m-2" src="/images/tool.svg" alt="" srcset="">
                         </div>
                     </div>
-                    <div class="border-0 p-4 w-full sm:w-1/3 m-2 rounded-xl shadow-md bg-slate-50 flex-none">
+                    <div class="border-0 p-4 w-full sm:w-1/3 m-2 rounded-xl shadow-md bg-slate-700 flex-none">
                         <b>Historical data</b>
                         <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, porro?</p>
                         <div class="justify-center flex text-center w-full">
-                            <img class="select-none" src="/images/bar-chart-2.svg" alt="" srcset="">
+                            <img class="select-none m-2" src="/images/bar-chart-2.svg" alt="" srcset="">
                         </div>
                     </div>
                 </div>
@@ -46,7 +46,7 @@
             <div id="prices">
                 <h3 class="text-xl"><b>Pricing</b></h3>
                 <div class="w-5/6 sm:w-2/3 m-auto sm:flex flex-column sm:flex-row justify-center">
-                    <div class="bg-white my-2  sm:my-2 sm:mx-2 p-2 w-full sm:w-1/2 flex-none shadow-md rounded-xl bg-slate-50">
+                    <div class="dark:bg-slate-700 my-2  sm:my-2 sm:mx-2 p-2 w-full sm:w-1/2 flex-none shadow-md rounded-xl bg-slate-50">
                         <p class="text-lg"><b>Basic</b></p>
                         <div class="text-left">
                             <p><img class="inline m-1 select-none" src="/images/check.svg" alt=""/>Track any product</p>
@@ -58,7 +58,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="bg-white my-2  sm:my-2 sm:mx-2 p-2 w-full sm:w-1/2 flex-none shadow-md rounded-xl bg-slate-50">
+                    <div class="dark:bg-slate-700 my-2  sm:my-2 sm:mx-2 p-2 w-full sm:w-1/2 flex-none shadow-md rounded-xl bg-slate-50">
                         <p class="text-lg"><b>Premium</b></p>
                         <div class="text-left">
                             <p><img class="inline m-1 select-none" src="/images/check.svg" alt=""/>More frequent price updates</p>
