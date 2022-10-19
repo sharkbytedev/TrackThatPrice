8da756a38f9b934c5cba97c2edad142ee0d494a5
Ethan Ward-Gale <ethangale01@gmail.com>
2022-10-19 09:06:58 -0400

    Trackers page made operational, new trackers page mostly functional, tailwind css implemented but doesn't work for me

--- resources/views/create-new-tracker.blade.php
+++ resources/views/create-new-tracker.blade.php
@@ -15,9 +15,9 @@ if (Auth::check() == false){
 Product URL: <input type="text" name="productURL"><br>
 Store: <select name="store">
 	<option value="">Select a store</option>
-	<option value="Amazon">Amazon</option>
-	<option value="Ebay">Ebay</option>
-	<option value="Wayfair">Wayfair</option>
+	<option value="amazon">Amazon</option>
+	<option value="ebay">Ebay</option>
+	<option value="wayfair">Wayfair</option>
 </select><br>
 <input type="submit" value="Submit" name="Submit1">
 </form> 
@@ -25,31 +25,31 @@ Store: <select name="store">
 <?php
 $product = new App\Models\Product();
 if(isset($_GET["Submit1"])){
-    //$handler = App\ProductHandlers\ProductHandlerFactory::new($product);
-    //$product_details = $handler::crawl($product);
-
-    $product->product_url = $_GET["productURL"];
-    $product->product_name = "default"; //after merging with product handler branch, remove this and replace it with the commented code underneath it
-    //$product->product_name = $product_details->name;
-    $product->update_interval = 0;
     $product->store = $_GET["store"];
-    //after merging with product handler branch, uncomment the code below
-    //if($product_details->image_url != null){
-    //    $product->image_url = $product_details->image_url;
-    //}
-    //else{
-    //    $product->image_url = "https://cdn.discordapp.com/attachments/620091571521454082/1027339673225396285/unknown.png"; //this is a placeholder
-    //}
-    //$product->price = $product_details->price;
+    $product->product_url = $_GET["productURL"];
+
+    $handler = App\ProductHandlers\ProductHandlerFactory::new($product);
+    $product_details = $handler::crawl($product);
+
+    if($product_details->name != null){
+        $product->product_name = $product_details->name;
+    }
+    else{
+        $product->product_name = "Product";
+    }
+    $product->update_interval = 0;
+    if($product_details->image_url != null){
+        $product->image_url = $product_details->image_url;
+    }
+    else{
+        $product->image_url = "https://cdn.discordapp.com/attachments/620091571521454082/1027339673225396285/unknown.png"; //this is a placeholder
+    }
+    $product->price = $product_details->price;
     $product->save();
     $lastid = $product->id;
     Auth::user()->products()->attach($lastid);
     
-    
-    echo $product->product_url;
-    echo $product->product_name;
-    echo $product->update_interval;
-    echo $product->store;
+    echo ($product->product_name . " tracker created successfully!");
 }
 
 ?>
--- resources/views/trackers.blade.php
+++ resources/views/trackers.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <body>
+<title> Trackers - TrackThatPrice </title>
 <?php 
 use Illuminate\Support\Facades\Auth;
 
@@ -10,14 +11,40 @@ if (Auth::check() == false){
 //this will redirect the user to the login page if they arent already logged in.
 //change localhost:8000/login to actual login page later
 ?>
+<h1>
+    Your Trackers:
+</h1>
 
-All Trackers:
+<br>
 <?php 
-$UserProducts = auth()->user()->products()->get();
-if(count($UserProducts) > 0){
-    for ($i = 0; $i < count($UserProducts); $i++){ 
-        ?> <br> <?php
-        echo $UserProducts[$i]->product_name;
+$userProducts = auth()->user()->products()->get();
+if(count($userProducts) > 0){
+    for ($i = 0; $i < count($userProducts); $i++){
+        ?>
+        <div class="border-solid border-2 border-blue-900 rounded p-1 mx-auto max-x-3xl">
+            <img src="<?php echo $userProducts[$i]->image_url;?>" class="border-solid border-2 border-blue-900 float-left w-32 h-32 object-contain">
+
+            <p class="text-xl max-x-xl"> <!-- Large product name text -->
+                <?php 
+                if(strlen($userProducts[$i]->product_name) < 75){
+                    echo $userProducts[$i]->product_name; 
+                }
+                else{
+                    echo (substr($userProducts[$i]->product_name, 0, 75). "...");
+                }
+                ?> <br>
+            </p>
+
+            <p> 
+                <?php
+                echo $userProducts[$i]->store; ?> <br> <?php
+                echo $userProducts[$i]->price; ?> <br> <?php
+                //echo $userProducts[$i]->product_url;
+                ?>
+            </p>
+        </div>
+        <br>
+        <?php
     }
 }
 else{
