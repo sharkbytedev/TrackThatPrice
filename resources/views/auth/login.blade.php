c8130c463aca4f8a2d6f1866f190423506b1877d
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-10-20 10:11:32 -0400

    Added dark mode

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout class="h-full">
-    <div class="w-full h-full flex">
-        <div class="w-10/12 lg:w-1/3  m-auto shadow rounded-xl p-5 bg-slate-50">
+    <div class="w-full h-full flex dark:bg-slate-800 dark:text-slate-200">
+        <div class="w-10/12 lg:w-1/3 m-auto shadow rounded-xl p-5 bg-slate-50 dark:bg-slate-700">
             <h1 class="text-center w-full text-xl m-b"><b>Login</b></h1>
             <br>
             <form action="{{ route('login') }}" method="POST">
@@ -10,7 +10,7 @@
                 <div class="flex-col justify-center flex">
                     <x-text-input 
                         id="email" 
-                        class="block mb-2 h-10" 
+                        class="block dark:placeholder:text-slate-200 dark:bg-slate-700 mb-2 h-10" 
                         placeholder="Email" type="email" 
                         name="email" :value="old('email')" 
                         required 
@@ -18,7 +18,7 @@
                     />
                     <x-text-input 
                         id="password" 
-                        class="block mt-1 h-10 w-full"
+                        class="block dark:placeholder:text-slate-200 dark:bg-slate-700 mt-1 h-10 w-full"
                         placeholder="Password"
                         type="password"
                         name="password"
@@ -28,7 +28,7 @@
                     <div class="block mt-4">
                         <label for="remember_me" class="inline-flex items-center">
                             <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                            <span class="ml-2 text-sm dark:text-slate-200">{{ __('Remember me') }}</span>
                         </label>
                     </div>
                     <div class="flex items-center justify-center mt-4">
@@ -40,14 +40,14 @@
                     <div class="w-full flex-row flex">
                         <div class="w-1/2 flex justify-start">
                             @if (Route::has('password.request'))
-                                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                                <a class="underline text-sm dark:text-slate-200 dark:hover:text-slate-300 text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                                     {{ __('Forgot your password?') }}
                                 </a>
                             @endif
                         </div>
                         <div class="w-1/2 justify-end flex">
                             @if (Route::has('register'))
-                                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                                <a class="underline text-sm dark:text-slate-200 dark:hover:text-slate-300 text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
                                     {{ __('Don\'t have an account?') }}
                                 </a>
                             @endif
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout class="h-full">
-    <div class="w-full h-full flex">
-        <div class="w-10/12 lg:w-1/3  m-auto shadow rounded-xl p-5 bg-slate-50">
+    <div class="w-full h-full flex dark:bg-slate-800 dark:text-slate-200">
+        <div class="w-10/12 lg:w-1/3  m-auto shadow rounded-xl p-5 dark:bg-slate-700 bg-slate-50">
             <h1 class="text-center w-full text-xl m-b"><b>Register</b></h1>
             <br>
             <form action="{{ route('register') }}" method="POST">
@@ -8,10 +8,10 @@
                 <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
                 <div class="flex-col justify-center flex">
-                    <x-text-input id="name" class="block mb-2 w-full" type="text" name="name" :value="old('name')" placeholder="Name" required autofocus />
+                    <x-text-input id="name" class="dark:bg-slate-700 dark:placeholder:text-slate-200 block mb-2 w-full" type="text" name="name" :value="old('name')" placeholder="Name" required autofocus />
                     <x-text-input 
                         id="email" 
-                        class="block mb-2 h-10" 
+                        class="dark:bg-slate-700 dark:placeholder:text-slate-200 block mb-2 h-10" 
                         placeholder="Email" type="email" 
                         name="email" :value="old('email')" 
                         required 
@@ -19,7 +19,7 @@
                     />
                     <x-text-input 
                         id="password" 
-                        class="block mb-2 h-10 w-full"
+                        class="dark:bg-slate-700 dark:placeholder:text-slate-200 block mb-2 h-10 w-full"
                         placeholder="Password"
                         type="password"
                         name="password"
@@ -28,7 +28,7 @@
                     />
                     <x-text-input 
                         id="password_confirmation" 
-                        class="block w-full mb-2"
+                        class="dark:bg-slate-700 dark:placeholder:text-slate-200 block w-full mb-2"
                         type="password"
                         placeholder="Confirm password"
                         name="password_confirmation" 
@@ -44,7 +44,7 @@
                     <div class="w-full flex-row flex">
                         <div class="w-1/2 justify-start flex">
                             @if (Route::has('login'))
-                                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                                <a class="underline text-sm dark:text-slate-200 dark:hover:text-slate-300 text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                                     {{ __('Already have an account?') }}
                                 </a>
                             @endif
