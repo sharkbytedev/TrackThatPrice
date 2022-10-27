06f6d82fa42f8a66c718485efdac67a399562b03
Ethan Ward-Gale <ethangale01@gmail.com>
2022-10-27 11:47:16 -0400

    ran vendor\bin\pint to fix styling issues

--- routes/web.php
+++ routes/web.php
@@ -23,25 +23,23 @@ Route::get('/dashboard', function () {
 
 Route::match(['get', 'post'], '/trackers/new', function () {
     $product = new App\Models\Product();
-    if(isset($_POST["Submitted"])){
-        $product->store = $_POST["store"];
-        $product->product_url = $_POST["productURL"];
+    if (isset($_POST['Submitted'])) {
+        $product->store = $_POST['store'];
+        $product->product_url = $_POST['productURL'];
 
         $handler = App\ProductHandlers\ProductHandlerFactory::new($product);
         $product_details = $handler::crawl($product);
 
-        if($product_details->name != null){
+        if ($product_details->name != null) {
             $product->product_name = $product_details->name;
-        }
-        else{
-            $product->product_name = "Product";
+        } else {
+            $product->product_name = 'Product';
         }
 
-        if($product_details->image_url != null){
+        if ($product_details->image_url != null) {
             $product->image_url = $product_details->image_url;
-        }
-        else{
-            $product->image_url = "https://cdn.discordapp.com/attachments/620091571521454082/1027339673225396285/unknown.png"; //this is a placeholder
+        } else {
+            $product->image_url = 'https://cdn.discordapp.com/attachments/620091571521454082/1027339673225396285/unknown.png'; //this is a placeholder
         }
 
         $product->price = $product_details->price;
@@ -50,17 +48,17 @@ Route::match(['get', 'post'], '/trackers/new', function () {
         Auth::user()->products()->attach($lastid);
 
         $productCreated = true;
-    }
-    else{
+    } else {
         $productCreated = false;
     }
 
-    return view('trackers/new', ["productCreated" => $productCreated], ["product" => $product]);
+    return view('trackers/new', ['productCreated' => $productCreated], ['product' => $product]);
 })->middleware(['auth'])->name('trackers/new');
 
 Route::get('/trackers', function () {
     $products = auth()->user()->products()->get();
-    return view('trackers', ["products" => $products]);
+
+    return view('trackers', ['products' => $products]);
 })->middleware(['auth'])->name('trackers');
 
 require __DIR__.'/auth.php';
