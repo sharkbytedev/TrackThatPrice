f6709f8bad94955511743f7839b9cb4ce97ea309
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-10-27 10:14:49 -0400

    Added trackers.remove route

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,20 @@ Route::match(['get', 'post'], '/trackers/{product_id}', function (Request $reque
     $product = $user->products()->find($product_id);
 
     return isset($product) ? view('view-tracker', ['product' => $product]) : redirect('/dashboard');
-})->middleware(['auth']);
+})->middleware(['auth'])->name('trackers.view');
+
+Route::match(['get', 'post'], '/trackers/{product_id}/remove', function (Request $request, string $product_id) {
+    /** @var App\Models\User */
+    $user = Auth::user();
+    $product = $user->products()->find($product_id);
+
+    if ($request->isMethod('post')) {
+        $user->products()->detach($product->product_id);
+
+        return redirect('/dashboard');
+    }
+
+    return isset($product) ? view('delete-tracker', ['product' => $product]) : redirect('/dashboard');
+})->middleware(['auth'])->name('trackers.remove');
 
 require __DIR__.'/auth.php';
