16e3b391d33de4320ea94b555ec2b815875a3c13
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-10-31 09:33:31 -0400

    UpdateProductData job now sets store_id

--- app/Jobs/UpdateProductData.php
+++ app/Jobs/UpdateProductData.php
@@ -33,6 +33,7 @@ class UpdateProductData implements ShouldQueue
             $this->product->product_name = $details->name;
             $this->product->price = $details->price;
             $this->product->image_url = $details->image_url;
+            $this->product->store_id = $details->store_id;
             $this->product->save();
         }
         // TODO: Some way to notify users after a product they are tracking errors
