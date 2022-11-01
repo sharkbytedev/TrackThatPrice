017731f8240e683ac2f7c6554f209286a729e074
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-11-01 09:26:00 -0400

    Updated models with new pivot attributes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,6 +24,6 @@ class Product extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class, 'product_user')->withTimestamps();
+        return $this->belongsToMany(User::class, 'product_user')->withTimestamps()->withPivot('enabled', 'type', 'threshold');
     }
 }
