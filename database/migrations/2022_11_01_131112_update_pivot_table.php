3b5c79d78df95aa34ee23e1ff5763042611af58f
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-11-01 09:20:17 -0400

    Updated pivot table

---
+++ database/migrations/2022_11_01_131112_update_pivot_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('product_user', function(Blueprint $table) {
+            $table->integer('threshold')->nullable();
+            $table->string('type', 10)->nullable();
+            $table->boolean('enabled');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('product_user', function(Blueprint $table) {
+            $table->dropColumn('threshold');
+            $table->dropColumn('type');
+            $table->dropColumn('enabled');
+        });
+    }
+};
