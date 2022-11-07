f0260e0822356fbb2f6bc48e5a0f78ba85776955
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-11-07 10:28:47 -0500

    Fixed price difference calculation

--- app/Jobs/CheckProductPrices.php
+++ app/Jobs/CheckProductPrices.php
@@ -28,7 +28,6 @@ class CheckProductPrices implements ShouldQueue
         foreach ($users as $user) {
             // Get the price closest to the compare time
             /** @var int */
-            Log::debug($user->pivot->compare_time);
             $h = $this->product->history()
                 ->where('created_at', '<=', $user->pivot->compare_time)
                 ->orderBy('created_at', 'desc')
@@ -41,15 +40,16 @@ class CheckProductPrices implements ShouldQueue
                 // No data for before the compare time
                 continue;
             }
+
             if ($user->pivot->type === 'flat') {
-                if ($this->product->price - $last_price >= $user->pivot->threshold) {
-                    NotifyUser::dispatch($user, 'flat decrease');
+                if ($last_price - $this->product->price >= $user->pivot->threshold) {
+                    NotifyUser::dispatch($user, 'flat', $last_price - $this->product->price, $last_price, $this->product);
                 }
             }
             else if ($user->pivot->type === 'percent') {
-                $dropped_percent = (($this->product->price - $last_price) / $last_price) * 100.0;
+                $dropped_percent = (($last_price - $this->product->price) / $last_price) * 100.0;
                 if ($dropped_percent >= $user->pivot->threshold) {
-                    NotifyUser::dispatch($user, 'percent decrease');
+                    NotifyUser::dispatch($user, 'percent', $dropped_percent, $last_price, $this->product);
                 }
             }
         }
