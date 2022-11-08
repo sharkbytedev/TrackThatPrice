c32a8c0af7fe85e8396f6d445e393daaf7a7d3e3
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-11-08 10:40:55 -0500

    Made enabled nullable

--- database/migrations/2022_11_01_131112_update_pivot_table.php
+++ database/migrations/2022_11_01_131112_update_pivot_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::table('product_user', function (Blueprint $table) {
             $table->integer('threshold')->nullable();
             $table->string('type', 10)->nullable();
-            $table->boolean('enabled');
+            $table->boolean('enabled')->nullable();
             $table->timestamp('compare_time')->nullable();
         });
     }
