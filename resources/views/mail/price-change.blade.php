444d10ef77a22ba15a41ec470f56efb236fba970
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-11-09 09:07:42 -0500

    Changed email text size

--- resources/views/mail/price-change.blade.php
+++ resources/views/mail/price-change.blade.php
@@ -1,5 +1,5 @@
 <div>
     <h3>A product you're tracking has had a price drop.</h3>
-    <p>A product you're tracking, <a href="{{ $product->product_url }}">"{{ $product->product_name }}"</a>, has had a price drop of {{ $change_type == 'flat' ? $change_value/100.0 : $change_value.'%' }}</p>
-    <p><del style="color:red;">{{ $old_price/100.0 }}</del> -> <b style="color: green;">{{ $product->price/100.0 }}</b></p>
+    <h5>A product you're tracking, <a href="{{ $product->product_url }}">"{{ $product->product_name }}"</a>, has had a price drop of {{ $change_type == 'flat' ? $change_value/100.0 : $change_value.'%' }}</h5>
+    <h5><del style="color:red;">{{ $old_price/100.0 }}</del> -> <b style="color: green;">{{ $product->price/100.0 }}</b></h5>
 </div>
