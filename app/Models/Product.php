1bd0d7e1a6d62211b04830685e89aff750528eac
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-11-11 10:42:07 -0500

    Added withPivot for pivot values

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -29,6 +29,6 @@ class Product extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class, 'product_user')->withTimestamps();
+        return $this->belongsToMany(User::class, 'product_user')->withTimestamps()->withPivot('type', 'threshold', 'compare_time');
     }
 }
