69b40e8fedadb67515c7a113c5bbb04dba612b26
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-11-15 11:03:55 -0500

    Added settings button

--- resources/views/view-tracker.blade.php
+++ resources/views/view-tracker.blade.php
@@ -8,6 +8,9 @@
                 <button class="float-right p-1 hover:bg-slate-200 rounded" type="button">
                     <img src="/images/trash-2.svg" width="20" height="20"  alt="Delete tracker">
                 </button>
+                <button class="float-right p-1 hover:bg-slate-200 rounded">
+                    <img src="/images/settings.svg" width="20" height="20" alt="Update tracker">
+                </button>
             </div>
         </div>
     </x-slot>
