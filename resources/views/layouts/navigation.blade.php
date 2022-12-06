49f4c43c931a1523263de572965c69d8b1e60035
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-12-06 10:59:28 -0500

    Removed laravel logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <h3>Track That Price</h3>
+                        {{-- <x-application-logo class="block h-10 w-auto fill-current text-gray-600" /> --}}
                     </a>
                 </div>
 
