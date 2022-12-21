8bd30abe2f7eb5d7ed61116b45d41a3ca762ccb1
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-12-21 11:30:34 -0500

    Moved archived button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,11 +21,18 @@
 @if (count($products) > 0)
     <div class="w-full md:w-2/3 m-auto">
         @foreach($products as $product)
-            @if($product->pivot->archived == false)
-                <x-product-card :product="$product" />
-            @endif
+            <x-product-card :product="$product" />
         @endforeach
-        {{ $products->links() }}
+        <div class="w-full flex align-middle">
+            <div class="w-4/5 inline">
+                {{ $products->links() }}
+            </div>
+            <div class="inline h-full align-middle">
+                <a href="{{ route('trackers.archived') }}">
+                    <button class="text-xs bg-slate-200 mt-1 mx-2 p-2">Archived trackers ({{ $archived_count }})</button>
+                </a>
+            </div>
+        </div>
     </div>
 @else
     <p class="justify-items-center grid"> No trackers found! Create one using the "Create A Tracker" button! </p>
