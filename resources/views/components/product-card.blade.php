e5251557f008fb544ed9bb1d9720f97f9eccf295
Momo8289 <42454181+Momo8289@users.noreply.github.com>
2022-12-21 09:36:16 -0500

    Updated price display

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -4,32 +4,37 @@
     @else
         <img src="/images/ImagePlaceholder.svg" class="justify-self-center bg-slate-200 rounded-xl w-32 h-32 object-contain break-after-column mt-1 md:mt-0 md:mr-1 md:float-left grow-0 shrink-0" alt="Image not found!">
     @endif
-    <div class="justify-items-center grid md:grow md:items-center md:justify-items-start p-2">
-        <p class="flex text-xl md:max-w-xl text-center md:text-left {{$product->valid ? "" : "text-red-600"}}"> <!-- Large product name text -->
-            @if(mb_strlen($product->pivot->tracker_name) < 65)
-                {{$product->pivot->tracker_name}} 
-            @else
-                {{(mb_substr($product->pivot->tracker_name, 0, 65). "...")}}
-            @endif
-
-            @if($product->valid != true)
-            <span class="inline-flex">
-                <img src="/images/alert-triangle.svg" class="w-6 h-6" title="Tracker is missing information.  Product link may be invalid.">
-            </span>
-        @endif
-        </p>
-
-        <p class="text-3xl self-center md:ml-auto md:mr-0 md:text-xl">
-            @if($product->price != null)
-                @if(strlen($product->price) < 7)
-                    {{$product->price / 100 . "$"}}
+    <div class="justify-items-center grid md:grow md:items-center md:justify-items-start px-2">
+        <div class="w-full flex relative mt-2">
+            <p class="w-3/4 text-xl max-w-xl text-left inline truncate {{$product->valid ? "" : "text-red-600"}}"> <!-- Large product name text -->
+                @if (isset($product->pivot->tracker_name))
+                    {{ $product->pivot->tracker_name }}
+                @elseif (isset($product->product_name))
+                    {{ $product->product_name }}
                 @else
-                    {{(substr($product->price / 100, 0, 7). "...$")}}
+                    Name not specified
                 @endif
-            @else
-                N/A
-            @endif
-        </p>
+    
+                @if($product->valid != true)
+                    <span class="inline">
+                        <img src="/images/alert-triangle.svg" class="w-6 h-6" title="Tracker is missing information.  Product link may be invalid.">
+                    </span>
+                @endif
+            </p>
+    
+            <p class="w-1/4 mr-1 self-center text-xl absolute right-0 text-right inline">
+                @if($product->price != null)
+                    @if(strlen($product->price) < 7)
+                        {{$product->price / 100 . "$"}}
+                    @else
+                        {{(substr($product->price / 100, 0, 7). "...$")}}
+                    @endif
+                @else
+                    N/A
+                @endif
+            </p>
+
+        </div>
 
         <a href="{{ route('trackers.view', ['product_id'=>$product->id]) }}" class="flex-none">
             <x-primary-button class="flex-none  dark:border-slate-200 mr-1">
