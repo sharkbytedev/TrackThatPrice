0a936de425c27df3430fd785f411360aaf9390ec
Momo8289 <nicholashg8289@gmail.com>
2023-01-16 09:55:01 -0500

    Updated user agent

--- puppet.js
+++ puppet.js
@@ -9,6 +9,7 @@ const options = yargs.option("h", {alias: "html", type: "string"}).argv;
 const renderHtml = async (urls) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36");
     
     output = {}
     for (const url of urls) {
